223282a8be883aaad358e66f99b3ca6091b31c06
Kelby Valenti <kvalenti@vmware.com>
2018-03-20 11:57:16 -0400

    Sample code examples for about_page and ticketing
    Signed-off-by: Kelby Valenti <kvalenti@vmware.com>

---
+++ ui/samples/ticketing/ticketing_phase5/src/main/index.ts
@@ -0,0 +1,5 @@
+/** vcloud-director-ui-extension-sample-ticketing-phase5
+ *  SPDX-License-Identifier: BSD-2-Clause
+ *  Copyright 2018 VMware, Inc. All rights reserved. VMware Confidential
+ */
+export {TicketingModule} from './ticketing.module';
