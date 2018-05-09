ebb9fa7cac78b597aeac8c2603029c410f6b9665
Jeff Moroski <jeff.moroski@gmail.com>
2018-05-09 16:09:30 -0400

    Add utility to build custom vCD themes. (#11)
    This change adds a tool that allows the user to define color and
    selector overrides for the default Clarity styles that vCloud Director
    uses by default.
    
    The Clarity dark theme is included as a full example of how the
    customization can be defined.
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

---
+++ ui/theme-generator/build-theme.js
@@ -0,0 +1,55 @@
+const ccss = require('clean-css');
+const fs = require('fs');
+const path = require('path');
+const sass = require('node-sass');
+const args = require('yargs');
+
+const argv = args.usage('$0 [options]', 'Generates a CSS file with specified overrides applied to base Clarity styles')
+    .options({
+        'theme': {
+            alias: 't',
+            demandOption: true,
+            describe: 'Theme directory to generate CSS for',
+            type: 'string'
+        },
+        'optimize': {
+            alias: 'o',
+            default: false,
+            describe: 'Whether or not to optimize and minimize the generated CSS',
+            type: 'boolean'
+        }
+    }).argv;
+
+sass.render({
+    file: `src/${argv.theme}/styles.scss`
+}, function(error, result) {
+    if (error) {
+        console.log(error.status);
+        console.log(error.column);
+        console.log(error.message);
+        console.log(error.line);
+    } else {
+        var output = result.css;
+        if (argv.optimize) {
+            const css = new ccss({
+                level: {
+                    1: {
+                        specialComments: 1
+                    },
+                    2: {
+                        restructureRules: true
+                    }
+                }
+            });
+            
+            output = css.minify(output).styles;
+        }
+        
+        if (!fs.existsSync(`${__dirname}/target`)) {
+            fs.mkdirSync(`${__dirname}/target`);
+        }
+        
+        fs.writeFileSync(`target/${argv.theme}.css`, output);
+        console.log(`Theme generated as ${fs.realpathSync(`target/${argv.theme}.css`)}`)
+  }
+})
