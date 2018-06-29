b2b4364c8505db086e6b4d17203787814a782556
Jeff Moroski <jmoroski@vmware.com>
2018-06-29 10:44:44 -0400

    Add the initial UI vCD SDK implementation.
    This change adds a new Angular module with a very basic API client
    experience.  When a project uses the new @vcd/sdk package, it can easily
    plumb vCloud Director API access to whatever components need it by
    leveraging the new VcdApiClient provider.  This initial commit
    introduces the ability to log in (or provide an existing bearer token)
    and perform some basic querying.  Obviously more functionality will be
    coming in the near future.  Refer to the api-client/README.md for usage
    info.
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

--- ui/api-client/packages/bindings/clean-bindings.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs-extra');
-
-if (fs.existsSync(`${__dirname}/lib`)) {
-    fs.removeSync(`${__dirname}/lib`);
-}
-
-if (fs.existsSync(`${__dirname}/dist`)) {
-    fs.removeSync(`${__dirname}/dist`);
-}
