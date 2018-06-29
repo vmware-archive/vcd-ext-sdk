c6fe03ef30ce62917f826a93e86fc717b8aafa09
Jeff Moroski <jmoroski@vmware.com>
2018-06-29 10:44:44 -0400

    Add the initial UI vCD SDK implementation.
    This change adds a new Angular module with a very basic API client
    experience.  When a project uses the new @vcd/sdk package, it can easily
    plumb vCloud Director API access to whatever components need it by
    leveraging the new VcdApiClient provider.  This initial commit
    introduces the ability to log in (or provide an existing bearer token)
    and perform some basic querying.  Obviously more functionality will be
    coming in the near future.  Refer to the api-client/README.md for usage
    info.
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

---
+++ ui/api-client/packages/sdk/src/http-interceptors/logging.interceptor.ts
@@ -0,0 +1,49 @@
+import { Injectable, Optional } from '@angular/core';
+import {
+  HttpEvent, HttpInterceptor, HttpHandler,
+  HttpRequest, HttpResponse
+} from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { finalize, tap } from 'rxjs/operators';
+import { ApiResultService, ApiResult } from '../api.result.service';
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+  private _outputToConsole: boolean;
+  private _enabled: boolean = false;
+  set enabled(enabled: boolean) {
+    this._enabled = enabled;
+    if (this._enabled && this._outputToConsole) {
+      console.warn('API logging enabled but no provider found for ApiResultService.  Results will be output to the console.');
+    }
+  }
+
+  constructor(@Optional() private apiResultService: ApiResultService) {
+    this._outputToConsole = !this.apiResultService;
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    if (!this._enabled) {
+      return next.handle(req);
+    }
+
+    const started = new Date();
+    let succeeded: boolean;
+
+    return next.handle(req)
+      .pipe(
+        tap(
+          event => succeeded = event instanceof HttpResponse ? true : false,
+          error => succeeded = false
+        ),
+        finalize(() => {
+          if (this._outputToConsole) {
+            console.log(`${req.method} ${req.urlWithParams} completed in ${Date.now() - started.getTime()} ms.  Success: ${succeeded}`);
+          } else {
+            this.apiResultService.add(new ApiResult(`${req.method} ${req.urlWithParams}`, succeeded, started, new Date()));
+          }
+        })
+      );
+  }
+}
