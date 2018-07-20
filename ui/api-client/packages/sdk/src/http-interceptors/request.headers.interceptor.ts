52ceba7060f29f263396806382a43c390626a680
Jeff Moroski <jmoroski@vmware.com>
2018-07-20 12:39:11 -0400

    Change Observable import to base 'rxjs' module.
    Without this change, the UI is unable to correctly bootstrap the plugin
    because the Observable reference conflicts with the modules that have
    been registered with SystemJS in the vCloud Director UI.
    
    Temporarily (?) removed catchError support because the rxjs empty()
    call doesn't currently work with the UI.
    
    Also added location parsing from the session.
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

--- ui/api-client/packages/sdk/src/http-interceptors/request.headers.interceptor.ts
+++ ui/api-client/packages/sdk/src/http-interceptors/request.headers.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpHandler, HttpInterceptor, HttpRequest, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class RequestHeadersInterceptor implements HttpInterceptor {
@@ -14,15 +14,17 @@ export class RequestHeadersInterceptor implements HttpInterceptor {
         this._version = _version;
     }
     
+    private _authenticationHeader: string = 'Authorization';
     private _authentication: string;
     set authentication(_authentication: string) {
         this._authentication = _authentication;
+        this._authenticationHeader = (this._authentication.length > 32) ? 'Authorization' : 'x-vcloud-authorization';
     }
     
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        let headers = req.headers.set('Accept', `application/*+json;version=${this._version}`);
+        let headers = req.headers.set('Accept', [`application/*+json;version=${this._version}`, `application/json;version=${this._version}`]);
         if (this._authentication) {
-            headers = headers.set('Authorization', `${this._authentication}`);
+            headers = headers.set(this._authenticationHeader, `${this._authentication}`);
         }
             
         const customReq: HttpRequest<any> = req.clone({
