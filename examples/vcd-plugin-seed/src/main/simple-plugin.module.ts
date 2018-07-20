8c8681c27916c07ca4264468b452cfb03d8110ce
Jeff Moroski <jmoroski@vmware.com>
2018-07-20 14:03:11 -0400

    Integrate the @vcd/sdk package into the seed project.
    This change includes the @vcd/sdk and @vcd/bindings packages into the
    seed project, and specifies them for inclusion in the rollup process.
    
    The SimpleModule has been updated to import the VcdSdkModule and use the
    VcdApiClient as a provider.  The module initialization will set the
    authentication on the client, allowing the SimpleComponent to display
    the logged in user and tenant.
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

--- examples/vcd-plugin-seed/src/main/simple-plugin.module.ts
+++ examples/vcd-plugin-seed/src/main/simple-plugin.module.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from "@angular/common";
-import {Inject, NgModule} from "@angular/core";
+import {Inject, NgModule, Optional} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
 import {ClarityModule} from "clarity-angular";
 import {Store} from "@ngrx/store";
-import {EXTENSION_ROUTE, ExtensionNavRegistration, ExtensionNavRegistrationAction, I18nModule} from "@vcd-ui/common";
+import {API_ROOT_URL, EXTENSION_ROUTE, ExtensionNavRegistration, ExtensionNavRegistrationAction, I18nModule, AuthTokenHolderService} from "@vcd-ui/common";
 import {SimpleComponent} from "./simple/simple.component";
+import { VcdApiClient, VcdSdkModule } from "@vcd/sdk";
 
 const ROUTES: Routes = [
     { path: "", component: SimpleComponent }
@@ -15,6 +16,7 @@ const ROUTES: Routes = [
         ClarityModule,
         CommonModule,
         I18nModule,
+        VcdSdkModule,
         RouterModule.forChild(ROUTES)
     ],
     declarations: [
@@ -22,16 +24,23 @@ const ROUTES: Routes = [
     ],
     bootstrap: [SimpleComponent],
     exports: [],
-    providers: []
+    providers: [VcdApiClient]
 })
 export class SimplePluginModule {
-    constructor(private appStore: Store<any>, @Inject(EXTENSION_ROUTE) extensionRoute: string) {
+    constructor(private appStore: Store<any>, @Inject(EXTENSION_ROUTE) extensionRoute: string, @Inject(API_ROOT_URL) private baseUrl: string, private client: VcdApiClient,
+            @Optional() private authToken: AuthTokenHolderService) {
         const registration: ExtensionNavRegistration = {
             path: extensionRoute,
             icon: "page",
             nameCode: "nav.label",
             descriptionCode: "nav.description"
         };
+
         appStore.dispatch(new ExtensionNavRegistrationAction(registration));
+
+        this.client.baseUrl = this.baseUrl;
+        if (this.authToken) {
+            this.client.setAuthentication(this.authToken.token).subscribe();
+        }
     }
 }
