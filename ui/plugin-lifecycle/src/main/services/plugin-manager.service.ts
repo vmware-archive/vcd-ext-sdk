c439c832f9a7c84bdc43191de8e553860656d6c7
Nikola Vladimirov Iliev <nvladimirovi@vmware.com>
2018-08-13 18:28:39 +0300

    Implement vcd api client create sync with observe response in enable plugin upload method
    Signed-off-by: Nikola Vladimirov Iliev <nvladimirovi@vmware.com>

--- ui/plugin-lifecycle/src/main/services/plugin-manager.service.ts
+++ ui/plugin-lifecycle/src/main/services/plugin-manager.service.ts
@@ -10,6 +10,7 @@ import { AuthTokenHolderService } from "@vcd/sdk/common";
 import { UiPluginMetadataResponse, UiPluginMetadata, EntityReference2 } from "@vcd/bindings/vcloud/rest/openapi/model";
 import { PluginService } from "./plugin.service";
 import { getPropsWithout } from "../helpers/object-helpers";
+import { HttpResponse } from "@angular/common/http";
 
 @Injectable()
 export class PluginManager {
