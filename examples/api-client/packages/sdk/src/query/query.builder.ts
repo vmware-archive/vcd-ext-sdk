330cce717368c98017d2aca8b8105414834b41be
Jeff Moroski <jeff.moroski@gmail.com>
2018-09-05 09:53:01 -0700

    Add filtering and field selection to Query.Builder. (#34)
    * Add filtering and field selection to Query.Builder.
    
    This change adds the ability to specify which fields of a query record
    to populate.  If specific fields are not requested (the default
    behavior), the entire record is returned.
    
    A Filter.Builder has also been added to support programmatic creation of
    filter conditions, using a fluent API style heavily influenced by the
    Apache CXF SearchConditionBuilder.  The builder supports creating
    conditions for equals, not equals, less than, less than or equal,
    greater than, greater than or equal, and wildcard equals (like), as well
    as conjunctions and disjunctions.  Strings, numbers, and booleans are
    currently supported where appropriate by the different comparison
    operations.
    
    Testing Done:
    
    Executed the following statement in a plugin:
    let builder = new Filter.Builder();
    let filter = builder.is('isEnabled').equalTo(true).and().or(
        builder.is('name').like('%aaaa', Filter.MatchMode.START),
        builder.is('name).like('org', Filter.MatchMode.START)
    ).query();
    this.client.query(Query.Builder.ofType('organization').links(false)
        .filter(filter).fields('displayName')).subscribe(console.log);
    
    Verified that the URL was built with a valid FIQL string, that the
    query did the appropriate level of filtering, and that only the
    displayName was populated in the respose records.
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

--- examples/api-client/packages/sdk/src/query/query.builder.ts
+++ examples/api-client/packages/sdk/src/query/query.builder.ts
@@ -1,9 +1,13 @@
+import { Filter } from './filter.builder';
+
 export namespace Query {
     export class Builder {
         private _type: string;
         private _format: Format = Format.ID_RECORDS;
         private _links: boolean = true;
         private _pageSize: number = 25;
+        private _fields: string[];
+        private _filter: string;
 
         private constructor() { }
 
@@ -28,8 +32,25 @@ export namespace Query {
             return this;
         }
 
+        public fields(...fields: string[]): Builder {
+            this._fields = fields;
+            return this;
+        }
+
+        public filter(filter: string): Builder {
+            this._filter = filter;
+            return this;
+        }
+
         public get(): string {
             let query: string = `?type=${this._type}&format=${this._format}&links=${this._links}&pageSize=${this._pageSize}`;
+            if (this._fields && this._fields.length > 0) {
+                query += `&fields=${this._fields.join(',')}`;
+            }
+
+            if (this._filter) {
+                query += `&filter=${this._filter}`;
+            }
 
             return query;
         }
