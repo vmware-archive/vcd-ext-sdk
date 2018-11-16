a0123487062c4700091449079d048a16db418764
dbyard-vmware <35938562+dbyard-vmware@users.noreply.github.com>
2018-11-16 10:20:23 -0800

    Scope HTTP client instance within SDK (#50)
    During research it was discovered that the currently-configured HttpClient is a singleton one, meaning
    that every single extension module and the container all use the same instance.  This could get
    hairy very quickly, as interceptors would also be global.
    
    This subclasses `HttpClient` as `VcdHttpClient`.  Within the extension and SDK, users can inject the VcdHttpClient, and because it is compiled in the module in will always have a unique reference per-extension and therefore provide the requisite module-level scoping.  It would also allow side by side injection with the standard `HttpClient` instance, which could then be used for non-vCD HTTP communication.
    
    Testing done: Built the test dashboard extension using this, and it still works properly.
    Signed-off-by: David Byard <dbyard@vmware.com>

--- examples/api-client/packages/sdk/src/index.ts
+++ examples/api-client/packages/sdk/src/index.ts
@@ -1,33 +1,41 @@
-import { ModuleWithProviders , NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { VcdApiClient } from './vcd.api.client';
-import { httpInterceptorProviders } from './http-interceptors';
+import {HttpClientModule} from "@angular/common/http";
+import {NgModule } from '@angular/core';
+import {CommonModule } from '@angular/common';
 
+import * as client from './client';
 import * as query from './query';
 import * as common from './common';
 import * as core from './core';
 import * as i18n from './i18n';
 
-export * from './vcd.api.client';
-export * from './api.result.service';
-
-export { 
+export {
+  client,
   common,
   core,
-  httpInterceptorProviders,
   i18n,
   query
 };
 
+// These exports, for legacy reasons, are exposed at the top level.
+const {VcdApiClient, ApiResultService} = client;
+export {VcdApiClient, ApiResultService};
+
+/**
+ * Extensions should import this module.  They can then wire in SDK components as desired.
+ */
 @NgModule({
-  imports: [
-    CommonModule,
-    HttpClientModule
-  ],
+  imports: [HttpClientModule, CommonModule],
   declarations: [i18n.TranslatePipe],
   exports: [i18n.TranslatePipe],
-  providers: [httpInterceptorProviders, VcdApiClient, i18n.TranslateService, i18n.TranslationLoader]
+  providers: [
+      client.RequestHeadersInterceptor,
+      client.LoggingInterceptor,
+      client.VcdHttpClient,
+      client.VcdApiClient,
+
+      i18n.TranslateService,
+      i18n.TranslationLoader
+  ]
 })
 export class VcdSdkModule {
 }
