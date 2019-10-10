556c7adca88515e996a10abbeb000dcb823dec97
Nikola Vladimirov Iliev <nikiviliev98@gmail.com>
2019-10-10 16:13:16 +0300

    Remove vcd-ext-sdk/ui/samples content. (#94)
    The folder had some samples that were not using current (or recent) best practices around SDK usage. They have been removed to reduce confusion. The main folder remains with a link to the new GitHub repository that has been dedicated to sample projects and utilities, and other sample code.
    
    Signed-off-by: Nikola Vladimirov Iliev <nvladimirovi@vmware.com>

---
+++ ui/samples/README.md
@@ -0,0 +1,2 @@
+# Sample vCD Portal Extension
+All the samples can be found in [vmware-samples/vcd-ext-samples](https://github.com/vmware-samples/vcd-ext-samples)
--- ui/samples/ticketing/ticketing_phase1/src/main/index.ts
+++
@@ -1,5 +0,0 @@
-/** vcloud-director-ui-extension-sample-ticketing-phase1
- *  SPDX-License-Identifier: BSD-2-Clause
- *  Copyright 2018 VMware, Inc. All rights reserved. VMware Confidential
- */
-export {TicketingModule} from './ticketing.module';
