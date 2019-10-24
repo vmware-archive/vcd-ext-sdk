43a67c8088ea0f2af81ebcc2546f77492168bd44
Nikola Vladimirov Iliev <nvladimirovi@vmware.com>
2019-10-24 13:36:03 +0300

    Implement angular schematic for plugin seed
    With this angular schematic the user will be
    able to generate plugin for specific vCloud Director
    version.
    
    - How to use it:
        - Install schematics globally: `npm i -g schematics`
        - Navigate to schematics directory: `cd ./ui`
        - Run: `nam pack ./vcd-plugin-seed-schematics`
        - The previous step will result in tar file you can install it locally or globally: `npm i <your_tar_file>` or `npm i -g <your_tar_file>`
        - Now you are able to generate plugin seed by running: `schematics vcd-plugin-seed-schematics:plugin-seed`
        - The expected result is:
            - Few questions about the plugin
            - New plugin created at you current work directory
    
    Testing Done:
    - Generate UI Plugin
    - Upload it to vcd cell
    - Verify the plugin is up and running
    
    Signed-off-by: Nikola Vladimirov Iliev <nvladimirovi@vmware.com>

---
+++ ui/vcd-plugin-seed-schematics/templates/9.1/__name@dasherize__/src/main/simple/simple.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject, OnInit } from "@angular/core";
+import { VcdApiClient } from '@vcd/sdk';
+import { EXTENSION_ASSET_URL } from '@vcd/sdk/common';
+import { Query } from "@vcd/sdk/query";
+import { Observable } from "rxjs";
+
+@Component({
+    selector: "plugin-simple",
+    templateUrl: "./simple.component.html",
+    styleUrls: ['./simple.component.scss'],
+    host: {'class': 'content-container'}
+})
+export class SimpleComponent implements OnInit {
+    username: Observable<string>;
+    tenant: Observable<string>;
+
+    constructor(@Inject(EXTENSION_ASSET_URL) public assetUrl: string, private client: VcdApiClient) {}
+
+    ngOnInit(): void {
+        this.tenant = this.client.organization;
+        this.username = this.client.username
+        
+        this.client.query(Query.Builder.ofType("organization").links(false)).subscribe(results => {
+            console.log(results);
+        })
+    }
+}
