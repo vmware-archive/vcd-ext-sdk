cad3435968d465d097df2507889f04ee3595c8e1
Jeff Moroski <jmoroski@vmware.com>
2020-04-01 11:57:34 -0400

    Remove plugin-lifecycle plugin.
    This was already deprecated and is the source of several security alerts. It can go away at this point.
    
    Signed-off-by: Jeff Moroski <jmoroski@vmware.com>

--- ui/plugin-lifecycle/http-transfer-service/dest/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { HttpClient } from '@angular/common/http';
-import { HttpTransferService } from "./http-transfer.service";
-export * from './http-transfer.service';
-export declare function transferServiceFactory(httpClient: HttpClient): HttpTransferService;
-export declare class VcdHttpTransferServiceModule {
-}
