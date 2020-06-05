64f042c0795eecef040cbffd97ad50d4b7b0e652
Nikola Vladimirov Iliev <nvladimirovi@vmware.com>
2020-06-05 09:59:32 +0300

    [VACE-2589] Create ng add plugin-seed-9.7-10.0
    Implement plugin seed for 9.7-10.0, the schematic will:
    - Update the angular.json file lazy modules property in the container
    service env.
    - Update the index file under the plugins directory in the container
    service env.
    
    Testing:
    - Seed plugin for 9.7-10.0 and verify it setups all the files
    described above.
    
    Signed-off-by: Nikola Vladimirov Iliev <nvladimirovi@vmware.com>

--- typescript/api-client/projects/vcd/schematics/src/schematics/plugin-seed/index.ts
+++ typescript/api-client/projects/vcd/schematics/src/schematics/plugin-seed/index.ts
@@ -1,21 +1,123 @@
-import { Rule, SchematicContext, Tree, url, apply, template, mergeWith } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, url, apply, template, mergeWith, chain, noop, SchematicsException, move } from '@angular-devkit/schematics';
 import { Schema } from "./schema";
 import { strings } from "@angular-devkit/core";
+import {
+  getWorkspace,
+  updateWorkspace
+} from '@schematics/angular/utility/config';
+import { normalize } from "path";
+import { getSourceFile } from "../utils";
+import { getSourceNodes } from "@schematics/angular/utility/ast-utils";
+import { SyntaxKind, SourceFile } from "@schematics/angular/third_party/github.com/Microsoft/TypeScript/lib/typescript";
+import { Change, InsertChange } from "@schematics/angular/utility/change";
+
+const PROJECT_NAME = "cloud-director-container";
+const PLUGINS_CONTAINING_FOLDER = "src/plugins";
+
+export function pluginSeed(options: Schema): Rule {
+  return chain([
+    createDirectoryAndFiles(options),
+    isVulcan(options.vcdVersion) ? noop() : isWellingtonOrXendi(options.vcdVersion) ? updateAngularJson(options) : noop(),
+    isVulcan(options.vcdVersion) ? noop() : isWellingtonOrXendi(options.vcdVersion) ? updatePluginRegistrations(options) : noop(),
+  ]);
+}
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function pluginSeed(_options: Schema): Rule {
+function createDirectoryAndFiles(options: Schema): Rule {
   return (_tree: Tree, _context: SchematicContext) => {
-    const { vcdVersion } = _options;
+    const { vcdVersion } = options;
 
     const sourceTemplates = url(`./templates/${vcdVersion}`);
     const sourceParametrizedTemplates = apply(sourceTemplates, [
       template({
-        ..._options,
+        ...options,
         ...strings
-      })
+      }),
+      move(normalize(PLUGINS_CONTAINING_FOLDER))
     ]);
 
     return mergeWith(sourceParametrizedTemplates);
   };
+}
+
+function updateAngularJson(options: Schema): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    const workspaceConfig = getWorkspace(tree);
+
+    if (!workspaceConfig) {
+      throw new SchematicsException('Could not find Angular workspace configuration');
+    }
+
+    if (!workspaceConfig.projects[PROJECT_NAME]) {
+      throw new SchematicsException(`Could not find Angular Project with name ${PROJECT_NAME} in your workspace`); 
+    }
+
+    if (!workspaceConfig.projects[PROJECT_NAME].architect) {
+      throw new SchematicsException('Could not find Angular architect configuration in your workspace');
+    }
+
+    let lazyModules: string[] = (workspaceConfig.projects as any)[PROJECT_NAME].architect.build.options.lazyModules;
+
+    if (!lazyModules || !lazyModules.length) {
+      lazyModules = [];
+    }
+
+    const moduleToAdd = `${PLUGINS_CONTAINING_FOLDER}/${options.name}/src/main/simple-plugin.module`;
+
+    if (lazyModules.indexOf(moduleToAdd) === -1) {
+      lazyModules.push(moduleToAdd);
+    }
+
+    (workspaceConfig.projects as any)[PROJECT_NAME].architect.build.options.lazyModules = lazyModules;
+
+    return updateWorkspace(workspaceConfig)(tree, context as any) as any;
+  }
+}
+
+function updatePluginRegistrations(options: Schema): Rule {
+  return (tree: Tree, _context: SchematicContext) => {
+    const pluginConfig = getSourceFile(tree, `${PLUGINS_CONTAINING_FOLDER}/index.ts`);
+
+    if (!pluginConfig) {
+      throw new SchematicsException(`File not found: ${pluginConfig}`);
+    }
+
+    const changes: Change[] = [
+      addNewPlugin(options.name, pluginConfig)
+    ];
+    
+    const recorder = tree.beginUpdate(`${PLUGINS_CONTAINING_FOLDER}/index.ts`);
+
+    changes.forEach((change: any) => {
+      if (change instanceof InsertChange) {
+        recorder.insertLeft(change.pos, change.toAdd);
+      }
+    });
+
+    tree.commitUpdate(recorder);
+
+    return tree;
+  }
+}
+
+function addNewPlugin(folderName: string, pluginConfig: SourceFile) {
+  const arr = getSourceNodes(pluginConfig).filter((node) => {
+    return node.kind === SyntaxKind.CloseBracketToken
+  });
+
+  const lastPluginItem = arr[arr.length - 1].getStart() - 1;
+
+  return new InsertChange(
+    `${PLUGINS_CONTAINING_FOLDER}/index.ts`,
+    lastPluginItem,
+    `\tnew PluginRegistration("src/plugins/${folderName}/src", "main/simple-plugin.module#SimplePluginModule", "Seed Plugin"),\n`)
+}
+
+function isVulcan(version: string) {
+  return version === "9.1";
+}
+
+function isWellingtonOrXendi(version: string) {
+  return version === "9.7" || version === "10.0";
 }
