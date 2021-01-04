8d77318cc83d5c8d5b76fbc38a27849cd68c553d
Milko Hristov Slavov <mslavov@vmware.com>
2021-01-04 10:50:37 +0200

    Reverting the github actions changes Fixing the UI seed template
    Signed-off-by: Milko Slavov <mslavov@vmware.com>

--- packages/care-package/uiplugins-plugin/templates/new/src/main/<%- name %>.module.ts
+++ packages/care-package/uiplugins-plugin/templates/new/src/main/<%- name %>.module.ts
@@ -3,8 +3,8 @@ import { Inject, NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { Store } from "@ngrx/store";
 import { VcdApiClient, VcdSdkModule } from "@vcd/sdk";
-import { ExtensionNavRegistration, EXTENSION_ROUTE, EXTENSION_ASSET_URL } from "@vcd/sdk/common";
-import { PluginModule } from "@vcd/sdk/core";
+import { EXTENSION_ASSET_URL } from "@vcd/sdk";
+import { PluginModule } from "@vcd/sdk";
 import { I18nModule, TranslationService } from "@vcd/i18n";
 import { ClarityModule } from "@clr/angular";
 import { <%- nameCamelCase %>Component } from "./<%- name %>/<%- name %>.component";
@@ -29,14 +29,8 @@ const ROUTES: Routes = [
     providers: [VcdApiClient]
 })
 export class <%- nameCamelCase %>PluginModule extends PluginModule {
-    constructor(appStore: Store<any>, @Inject(EXTENSION_ROUTE) extensionRoute: string, translationService: TranslationService) {
+    constructor(appStore: Store<any>, translationService: TranslationService) {
         super(appStore);
-        translationService.registerTranslations()
-        this.registerExtension(<ExtensionNavRegistration>{
-            path: extensionRoute,
-            icon: "page",
-            nameCode: "nav.label",
-            descriptionCode: "nav.description"
-        });
+        translationService.registerTranslations();
     }
 }
