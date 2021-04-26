443666ecd76066f63346a9f094ba7e63cadc4000
Milko Slavov <milko.slavov@gmail.com>
2021-04-26 14:49:21 +0300

    Update createSchema to include more validation (#201)
    * Update createSchema to include more validation
    - Added more test cases
    
    Testing Done
    run `vcd-ext new` locally and did not provide a solution name ->
        the result was error shown on the screen
    
    Signed-off-by: Milko Slavov <mslavov@vmware.com>
    
    Co-authored-by: Milko Hristov Slavov <mslavov@vmware.com>

--- packages/care-package/core/src/CarePackageGenerator.spec.ts
+++ packages/care-package/core/src/CarePackageGenerator.spec.ts
@@ -28,30 +28,35 @@ describe('CarePackageGenerator Tests', () => {
                 name: {
                     type: 'string',
                     description: 'Your solution name',
-                    required: true
+                    required: true,
+                    minLength: 1
                 },
                 version: {
                     type: 'string',
                     description: 'Specify first version',
                     default: '0.0.1',
-                    required: true
+                    required: true,
+                    minLength: 1
                 },
                 vendor: {
                     type: 'string',
                     description: 'Specify vendor name',
-                    required: true
+                    required: true,
+                    minLength: 1
                 },
                 link: {
                     type: 'string',
                     description: 'Specify vendor link',
                     default: 'http://example.com',
-                    required: true
+                    required: true,
+                    minLength: 1
                 },
                 license: {
                     type: 'string',
                     description: 'Specify solution license',
                     default: 'BSD-2-Clause',
-                    required: true
+                    required: true,
+                    minLength: 1
                 }
             }
         }));
--- packages/cli/src/prompt/JSONSchemaPrompt.spec.ts
+++ packages/cli/src/prompt/JSONSchemaPrompt.spec.ts
@@ -20,9 +20,53 @@ describe('JSONSchemaPrompt Tests', () => {
             default: undefined,
         }));
         expect(questions[0].validate).toBeDefined();
-        expect(() => {
-            // tslint:disable-next-line: no-unused-expression
-            questions[0].validate ? questions[0].validate('string_value') : undefined;
-        }).not.toThrow();
+        if (questions[0].validate) {
+            expect(questions[0].validate('string_value')).toBeTrue();
+            expect(questions[0].validate('')).toBeTrue();
+        }
     });
+    it('Generate question for required property', () => {
+        const testSchema = {
+            type: 'object',
+            properties: {
+                name: {
+                    type: 'string',
+                    description: 'name',
+                    minLength: 1
+                }
+            },
+            required: ['name']
+        };
+        const questions = JSONSchemaPrompt.convertToPromptQuestions(testSchema);
+        expect(questions.length).toBe(1);
+        expect(questions[0]).toEqual(jasmine.objectContaining({
+            name: 'name',
+            message: 'name',
+            default: undefined,
+        }));
+        expect(questions[0].validate).toBeDefined();
+        if (questions[0].validate) {
+            expect(questions[0].validate('string_value')).toBeTrue();
+            expect((questions[0].validate('') as string).indexOf('Please insert correct value')).toBeGreaterThan(-1);
+            expect((questions[0].validate(undefined) as string).indexOf('Please insert correct value')).toBeGreaterThan(-1);
+        }
+    });
+    it('Generate question with proper validation', () => {
+        const testSchema = {
+            type: 'object',
+            properties: {
+                name: {
+                    type: 'number',
+                    description: 'name'
+                }
+            }
+        };
+        const questions = JSONSchemaPrompt.convertToPromptQuestions(testSchema);
+        expect(questions[0].validate).toBeDefined();
+        if (questions[0].validate) {
+            expect((questions[0].validate('') as string).indexOf('Please insert correct value')).toBeGreaterThan(-1);
+            expect((questions[0].validate('string_value') as string).indexOf('Please insert correct value')).toBeGreaterThan(-1);
+        }
+    });
+
 });
