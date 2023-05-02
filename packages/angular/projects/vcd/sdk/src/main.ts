7f5504cab1de6f52cfa3ec673e2f096a5e4b6d1e
Iveta Ilieva <iilieva@vmware.com>
2023-05-02 14:09:31 +0300

    Issue: TranslateService and TranslatePipe are no longer needed. (#271)
    Fix: Remove TranslateService and TranslatePipe. The translation logic should be utilized from vcd/i18n package.
    
    Signed-off-by: Iveta Ilieva <iilieva@vmware.com>
    
    Signed-off-by: Iveta Ilieva <iilieva@vmware.com>
    Signed-off-by: Nikola Vladimirov Iliev <nvladimirovi@vmware.com>

--- packages/angular/projects/vcd/sdk/src/main.ts
+++ packages/angular/projects/vcd/sdk/src/main.ts
@@ -1,31 +1,27 @@
-import {HttpClientModule} from '@angular/common/http';
-import {NgModule } from '@angular/core';
-import {CommonModule } from '@angular/common';
-
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import * as client from '@vcd/angular-client';
 import * as common from './common';
 import * as core from './core';
-import * as i18n from './i18n';
 
 export {
   common,
   core,
-  i18n,
 };
 
 /**
- * Extensions should import this module.  They can then wire in SDK components as desired.
+ * Extensions should import this module.
+ * They can then wire in SDK components as desired.
  */
 @NgModule({
   imports: [
     HttpClientModule,
     CommonModule,
   ],
-  declarations: [i18n.TranslatePipe],
-  exports: [i18n.TranslatePipe],
+  declarations: [],
+  exports: [],
   providers: [
-    i18n.TranslateService,
-    i18n.TranslationLoader,
     client.RequestHeadersInterceptor,
     client.LoggingInterceptor,
     client.ResponseNormalizationInterceptor,
