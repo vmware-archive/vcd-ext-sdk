9a8dad017525c9abcca97a093cafe041b3bb8c4b
Nikola Vladimirov Iliev <nvladimirovi@vmware.com>
2023-05-09 11:01:46 +0300

    Delete @vcd/sdk schmatics
    Different way of seeding a UI Plugin will be used in
    future so we don't need the Angular Schematics.
    
    Signed-off-by: Nikola Vladimirov Iliev <nvladimirovi@vmware.com>

--- packages/angular/projects/vcd/sdk/src/schematics/ng-add/schema.d.ts
+++
@@ -1,8 +0,0 @@
-import { WorkspaceProject, ProjectType } from "@schematics/angular/utility/workspace-models";
-
-export interface Schema {
-    skipModuleImport: boolean;
-    moduleName: string;
-    importInModule: string;
-    project: WorkspaceProject<ProjectType.Application | ProjectType.Library>;
-}
